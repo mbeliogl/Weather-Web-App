801381b39a5cce3e6371b1185a7cec834125be1c
mbeliogl <maximbelioglo@gmail.com>
2023-01-14 16:23:51 -0500

    Updates

---
+++ precache-manifest.1271bc18565ec567403128bc26b2c616.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a51154865b29c17ab1bb",
+    "url": "%7Bmbeliogl%7D.github.io/%7BWeather-Web-App%7D/static/css/main.acffa17d.chunk.css"
+  },
+  {
+    "revision": "a51154865b29c17ab1bb",
+    "url": "%7Bmbeliogl%7D.github.io/%7BWeather-Web-App%7D/static/js/main.a5115486.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "%7Bmbeliogl%7D.github.io/%7BWeather-Web-App%7D/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "958ada1287af25d2a99f",
+    "url": "%7Bmbeliogl%7D.github.io/%7BWeather-Web-App%7D/static/js/2.958ada12.chunk.js"
+  },
+  {
+    "revision": "f71c4849eb09a0e17cdb",
+    "url": "%7Bmbeliogl%7D.github.io/%7BWeather-Web-App%7D/static/js/runtime~main.f71c4849.js"
+  },
+  {
+    "revision": "5c754b9cf9d4cd27952323eec61ee0ca",
+    "url": "%7Bmbeliogl%7D.github.io/%7BWeather-Web-App%7D/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "%7Bmbeliogl%7D.github.io/%7BWeather-Web-App%7D/precache-manifest.1271bc18565ec567403128bc26b2c616.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("%7Bmbeliogl%7D.github.io/%7BWeather-Web-App%7D/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
